58d4546c099bc0b1752cd1c89d0dd5411d62f277
Mike Bild <mike.bild@googlemail.com>
2017-09-29 10:38:03 +0200

    Add custom directives

--- graphql.js
+++ graphql.js
@@ -1,14 +1,7 @@
 import {pubsub} from './lib'
-
-export const channels = {
-  onItemChanged: (options, args) => ({
-    itemChangedChannel: {filter: event => true},
-  }),
-}
-
-export const loaders = {
-  items: [{id: 1}, {id: 2}, {id: 3}],
-}
+import fetch from 'node-fetch'
+import {GraphQLString, GraphQLInt} from 'graphql';
+import {DirectiveLocation} from 'graphql/type/directives';
 
 export const resolvers = {
   Query: {
@@ -22,5 +15,36 @@ export const resolvers = {
   },
   Subscription: {
     onItemChanged: (parent, args, context, info) => context.loaders.items,
+  },
+}
+
+export const loaders = {
+  items: [{id: 1}, {id: 2}, {id: 3}],
+}
+
+export const channels = {
+  onItemChanged: (options, args) => ({
+    itemChangedChannel: {filter: event => true},
+  }),
+}
+
+export const directives = {
+  fetch: {
+    description: 'fetch URL result value e.g. @fetch(url: "https://dropstack-mapping-example.services.dropstack.run/hellos")',
+    locations: [DirectiveLocation.FIELD],
+    args: {url: {type: GraphQLString}, timeout: {type: GraphQLInt}},
+    resolve: async (resolve, parent, args, ctx, info) => await resolve().then(() => fetch(args.url, {timeout: args.timeout || 0}).then(result => result.json())),
+  },
+  resolverTimeLog: {
+    description: 'Log resolver time',
+    locations: [DirectiveLocation.FIELD],
+    resolve: (resolve, parent, args, ctx, info) => {
+      const start = new Date();
+      return resolve().then(data => {
+        const diff = (new Date() - start);
+        console.log(`Log - resolver time [${info.parentType} / ${info.fieldName}]: ${diff}ms`)
+        return data;
+      })
+    }
   },
 }
